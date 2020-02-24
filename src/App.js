c4c682114664dc88d26fc83a40da9954ae5fa298
Akira360 <akira360.v@gmail.com>
2020-02-24 23:24:58 +0300

    +

--- src/App.js
+++ src/App.js
@@ -16,8 +16,26 @@ const App = () => {
       App !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
       App !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
       App !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      
+      
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          myProgram Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+
     </div>
   )
 }
 
 export default App
+
