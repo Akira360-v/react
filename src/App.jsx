cd352356919d3ce92828a55232277ab1e8f39397
Akira360 <akira360.v@gmail.com>
2020-02-25 20:49:46 +0300

    rt03

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,38 @@
 import React from 'react'
+// components app
 import Navbar from './components/app/Navbar'
 import Sidebar from './components/app/Sidebar'
-import './App.css'
-import './assets/sass/index.sass'
+// components app //
+// views
 import Employees from './views/Employees'
 import Equipment from './views/Equipment'
+import Statistics from './views/Statistics'
+import Settings from './views/Settings'
+// views //
+// Route
+import { BrowserRouter, Route  } from 'react-router-dom'
+// Route //
+// css
+import './assets/sass/index.sass'
+import './App.css'
+// css //
 
 const App = () => {
   return (
-    <div className="app-wrapper">
-      <Navbar />
-      <Sidebar />
-      <div className="app-wrapper-content">
-        <Employees />
-        <Equipment />
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Navbar />
+        <div className="app-wrappers">
+          <Sidebar />
+          <div className="app-wrapper-content">
+            <Route path='/Employees' component={Employees} />
+            <Route path='/Equipment' component={Equipment} />
+            <Route path='/Statistics' component={Statistics} />
+            <Route path='/Settings' component={Settings} />
+          </div>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   )
 }
 
